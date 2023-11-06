da2f336b9675f458f3bf03e0e1436344f0b728b5
Joe Heck <heckj@mac.com>
2023-11-06 13:20:00 -0800

    adds iOS .15 to platform list
    - fixes compilation errors when using this package within an iOS app

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RealityActions",
-    platforms: [.macOS(.v12), .visionOS(.v1)],
+    platforms: [.iOS(.v15), .macOS(.v12), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
